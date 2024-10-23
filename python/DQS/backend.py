5e5c54c7e3d0ec836e96a98a8da901b2e55e1f18
C6RVhWZTTBjcj3lqqcUdZjTArVXewnh95+8y/WxWYfw= <acarballido@login03.lan.cesga.es>
2024-10-23 10:44:26 +0200

    First steps

---
+++ python/DQS/backend.py
@@ -0,0 +1,14 @@
+ 
+
+
+
+
+
+class DistributeBackends:
+	
+	def __init__(self):
+		pass
+
+	def dist_backend(self, dict_backends_QPUs=0):
+		
+		print("Distribuimos los backends a los recursos")
---
+++ python/DQS/cluster.py
@@ -0,0 +1,24 @@
+"""
+Module to deploy cluster
+"""
+
+
+
+
+class Cluster:
+	
+	def __init__(self):
+		pass
+
+	def deploy(self, config_dict=0):
+		
+		print("Levantamos el cluster usando el fichero de config")
+
+	def is_deployed(self):
+	
+		print("El cluster esta levantado = True, en otro caso = False")
+
+	def info(self):
+		
+		print("Informacion sobre los recursos levantados")
+
