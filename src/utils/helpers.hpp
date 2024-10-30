efb06180a2882b08d243632511ef616d8c72e727
Jorge Vázquez Pérez <jorge.vazper@gmail.com>
2024-10-30 17:05:06 +0100

    Forgotten material from previous commit

---
+++ src/utils/helpers.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+template<typename TO, typename FROM>
+TO legacy_size_cast(FROM value)
+{
+    static_assert(std::is_unsigned_v<FROM> && std::is_unsigned_v<TO>,
+                  "Only unsigned types can be cast here!");
+    TO result = value;
+    return result;
+}
