539ffbb209e8c267ff18cb4d24cca4b6050c3298
Jorge Vázquez Pérez <jorge.vazper@gmail.com>
2024-11-12 15:06:07 +0100

    IP configuration and treatment completed

--- src/utils/ip_config.hpp
+++ src/utils/ip_config.hpp
@@ -1,38 +1,45 @@
 #pragma once
 
 #include <nlohmann/json.hpp>
-#include <boost/asio.hpp>
+#include <iostream>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
 
-using namespace boost::asio;
+#ifndef __USE_POSIX
+    #define __USE_POSIX
+#endif
+#include <limits.h>
 
+using namespace std::literals;
 using json = nlohmann::json;
+
+namespace ip_config {
 
 class IPConfig {
 public:
-
+    int qpu_id;
     std::string hostname;
-    std::vector<std::string> IPs;
+    std::unordered_map<std::string, std::string> IPs;
     std::string port;
 
-    IPConfig(const int& n_port)
-        : hostname{ip::host_name()},
-          IPs{_getIPAddresses()},
-          port{std::to_string(n_port)} { }
+    IPConfig(const int& qpu_id)
+        : qpu_id{qpu_id},
+          hostname{_get_hostname()},
+          IPs{_get_IP_addresses()},
+          port{_get_port()} { }
 
-    IPConfig(const std::string& hostname, const int& n_port)
-        : hostname{hostname},
-          IPs{_getIPAddresses()},
-          port{std::to_string(n_port)} { }
-    
-    IPConfig(const std::string& hostname, const std::string& n_port)
-        : hostname{hostname},
-          IPs{_getIPAddresses()},
-          port{n_port} { }
+    IPConfig(const int& qpu_id, const std::string& hostname)
+        : qpu_id{qpu_id},
+          hostname{hostname},
+          IPs{_get_IP_addresses()},
+          port{_get_port()} { }
     
 
-    IPConfig& operator=(const IPConfig& other) {
-        
-        // Check for self-assignment
+    IPConfig& operator=(const IPConfig& other) 
+    {
         if (this != &other) {
             hostname = other.hostname;
             IPs = other.IPs;
@@ -41,40 +48,98 @@ public:
         return *this;
     }
 
-    json to_json(const int& qpu_id){
-        json config_json = {
-            {std::to_string(qpu_id), {
-                { {"hostname", hostname}, {"port", port} },
-            }}
-        };
-        return config_json;
-    }
-
 private:
 
-    std::vector<std::string> _getIPAddresses() {
-        io_context io_context;
-        std::vector<std::string> ips;
+    std::string _get_hostname(){
+        char hostname[HOST_NAME_MAX];
+        gethostname(hostname, sizeof(hostname));
+        return hostname;
+    }
 
-        // Obtiene el nombre del host
-        std::string host = ip::host_name();
+    std::unordered_map<std::string, std::string> _get_IP_addresses() 
+    {
+        std::unordered_map<std::string, std::string> ips;
+        struct ifaddrs *interfaces, *ifa;
 
-        // Resuelve el nombre del host a direcciones
-        ip::tcp::resolver resolver(io_context);
-        ip::tcp::resolver::results_type endpoints = resolver.resolve(host, "");
-
-        for (const auto& endpoint : endpoints) {
-            ips.push_back(endpoint.endpoint().address().to_string());
+        if (getifaddrs(&interfaces) == -1) {
+            std::cerr << "Error getting the network interfaces\n";
+            return std::unordered_map<std::string, std::string>();
         }
+
+        for (ifa = interfaces; ifa != nullptr; ifa = ifa->ifa_next) {
+            if (ifa->ifa_addr == nullptr) continue;
+
+            int family = ifa->ifa_addr->sa_family;
+            if (family == AF_INET) {
+                char ip[INET6_ADDRSTRLEN];
+                void *addr;
+                
+                addr = &((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+                inet_ntop(family, addr, ip, sizeof(ip));
+
+                ips[ifa->ifa_name] = ip;
+            }
+        }
+
+        freeifaddrs(interfaces);
 
         return ips;
     }
+
+
+    // TODO: Hacer que el get_port no esté tan ligado a SLURM 
+    std::string _get_port() 
+    {
+        auto id = std::stoi(std::getenv("SLURM_LOCALID"));
+        std::string ports = std::getenv("SLURM_STEP_RESV_PORTS");
+
+        if(ports != "")
+        {
+            size_t pos = ports.find('-');
+            if (pos != std::string::npos) 
+            {
+                std::string base_port_str = ports.substr(0, pos);
+                int base_port = std::stoi(base_port_str);
+                
+                return std::to_string(base_port + id);
+                
+            } else 
+            {
+                std::cerr << "Not a valid expression format of the ports.\n";
+                return "-1"s;
+            }
+        } else 
+        {
+            std::cerr << "The required environment variables are not set (not).\n";
+            return "-1"s;
+        }
+    }
+
 };
 
-std::ostream& operator<<(std::ostream& os, const IPConfig& config) {
-    os << "\nIPs: ";
-    for (auto ip: config.IPs){
-        os << ip << " ";
+
+void to_json(json& j, const IPConfig& ip_config)
+    {
+        json aux{};
+
+        for (const auto& net_bind : ip_config.IPs) {
+            aux[net_bind.first] = net_bind.second;
+        }
+
+        j = {   
+                {"hostname", ip_config.hostname}, 
+                {"IPs", aux},
+                {"port", ip_config.port},
+            };
+    }
+};
+
+
+
+std::ostream& operator<<(std::ostream& os, const ip_config::IPConfig& config) {
+    os << "\nIPs: \n";
+    for (const auto& net_bind : config.IPs) {
+            os << net_bind.first << " ---> " << net_bind.second << "\n";
     }
     os << "\nPuerto: " << config.port
        << "\nHostname: " << config.hostname << "\n\n";
