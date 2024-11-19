d01ee36c69cc3ae33e01da74deb6c4d00c8e1a50
Jorge Vázquez Pérez <jorge.vazper@gmail.com>
2024-11-19 17:51:48 +0100

    Sparse checkout attempted to be solved

--- scripts/setup_submodules.sh
+++ scripts/setup_submodules.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
+cd ../
 
-# Script para configurar sparse checkout en submódulos
+if [ ! -f "../.gitmodules" ] || ! grep -q 'aer' "../.gitmodules"; then
+    git submodule add --name aer-cpp git@github.com:Qiskit/qiskit-aer.git src/third-party/aer-cpp
+fi
 
-# Definir los submódulos y sus rutas de sparse checkout
-declare -A submodule_sparse
-submodule_sparse["aer-cpp"]="../src/third-party/aer-cpp"
-
-# Iterar sobre cada submódulo y configurar sparse checkout
-for submodule in "${!submodule_sparse[@]}"; do
-    sparse_paths=${submodule_sparse[$submodule]}
-
-    # Definir la ruta del archivo sparse-checkout del submódulo
-    sparse_file="../.git/modules/$submodule/info/sparse-checkout"
-
-    # Habilitar sparse checkout en el submódulo
-    git -C "../$submodule" config core.sparseCheckout true
-
-    # Añadir las rutas especificadas al archivo sparse-checkout
-    for path in $sparse_paths; do
-        echo "$path" >> "$sparse_file"
-    done
-
-    # Aplicar la configuración de sparse checkout
-    git -C "../$submodule" read-tree -mu HEAD
-done
-
-echo "Sparse checkout configurado para los submódulos."
+cd src/third-party/aer-cpp/ && git config core.sparsecheckout true && cd ../../../
+echo src/ >> .git/modules/aer-cpp/info/sparse-checkout 
+cd src/third-party/aer-cpp/ && git read-tree -mu HEAD 
