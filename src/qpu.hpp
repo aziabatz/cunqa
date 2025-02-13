25bd81bfd64d39da53e523f1345eb061761b77dc
Jorge Vázquez Pérez <jorge.vazper@gmail.com>
2025-02-13 12:39:53 +0100

    Added option --all to qdrop and solved minor error

--- src/qpu.hpp
+++ src/qpu.hpp
@@ -57,6 +57,7 @@ QPU<sim_type>::QPU(config::QPUConfig<sim_type> qpu_config) :
 template <SimType sim_type>
 void QPU<sim_type>::turn_ON() 
 {
+    SPDLOG_LOGGER_INFO(logger, "We set up the server.");
     server = std::make_unique<Server>(qpu_config.net_config);
 
     std::thread listen([this](){this->_recv_data();});
@@ -72,6 +73,7 @@ void QPU<sim_type>::_compute_result()
     json kernel = {}; 
     while (true) 
     {
+        SPDLOG_LOGGER_INFO(logger, "We set up the lock for .");
         std::unique_lock<std::mutex> lock(queue_mutex_);
         queue_condition_.wait(lock, [this] { return !message_queue_.empty(); });
 
@@ -103,9 +105,9 @@ void QPU<sim_type>::_compute_result()
                         server->send_result(to_string(response));
                     }
                 } 
-            } catch(const boost::system::system_error& e) {
+            } catch(const ServerException& e) {
                 SPDLOG_LOGGER_ERROR(logger, "There has happened an error sending the result, probably the client has had an error.");
-                SPDLOG_LOGGER_ERROR(logger, "Boost message of the error: {}", e.what());
+                SPDLOG_LOGGER_ERROR(logger, "Message of the error: {}", e.what());
             } catch(const std::exception& e) {
                 SPDLOG_LOGGER_ERROR(logger, "There has happened an error sending the result, the server keeps on iterating.");
                 SPDLOG_LOGGER_ERROR(logger, "Message of the error: {}", e.what());
