efc65909145740fd6229d0a61f513fa43d1b454a
Jorge Vázquez Pérez <jorge.vazper@gmail.com>
2025-03-03 10:48:29 +0100

    Solved ZMQ dependencies in QMIO and FT3

--- scripts/jupyter.sh
+++ scripts/jupyter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
  
-#ml load qmio/hpc gcc/system gcccore/12.3.0 hpcx-ompi flexiblas/3.3.0 boost cmake/3.27.6 pybind11/2.12.0-python-3.9.9 nlohmann_json/3.11.3 ninja/1.9.0
-ml load cesga/2022 gcc/system flexiblas/3.3.0 openmpi/5.0.5 boost pybind11 cmake
-#module load qmio/hpc gcc/12.3.0 qiskit/1.2.4-python-3.9.9
-module load qiskit/1.2.4
+ml load qmio/hpc gcc/system gcccore/12.3.0 hpcx-ompi flexiblas/3.3.0 boost cmake/3.27.6 pybind11/2.12.0-python-3.9.9 nlohmann_json/3.11.3 ninja/1.9.0
+#ml load cesga/2022 gcc/system flexiblas/3.3.0 openmpi/5.0.5 boost pybind11 cmake
+module load qmio/hpc gcc/12.3.0 qiskit/1.2.4-python-3.9.9
+#module load qiskit/1.2.4
 export INSTALL_PATH=/mnt/netapp1/Store_CESGA/home/cesga/jvazquez/works/cunqa/installation
  
 export PATH=$PATH:$INSTALL_PATH/bin
