9493074664ba6f02646a0b2a86b8c4d627b2353e
Jorge Vázquez Pérez <jorge.vazper@gmail.com>
2025-03-07 09:46:53 +0100

    Update automatic configuration.

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,9 +3,8 @@
 if [ ! -z "$1" ]; then
     cd ../
     if [ $LMOD_SYSTEM_NAME == "QMIO" ]; then
-
         # Execution for QMIO
-        ml load qmio/hpc gcc/system gcccore/12.3.0 hpcx-ompi flexiblas/3.3.0 boost cmake/3.27.6 pybind11/2.12.0-python-3.9.9 nlohmann_json/3.11.3 ninja/1.9.0
+        ml load qmio/hpc gcc/12.3.0 hpcx-ompi flexiblas/3.3.0 boost cmake/3.27.6 pybind11/2.12.0-python-3.9.9 nlohmann_json/3.11.3 ninja/1.9.0 qiskit/1.2.4-python-3.9.9
         conda deactivate
         
         export INSTALL_PATH=$1
@@ -18,16 +17,13 @@ if [ ! -z "$1" ]; then
         # Execution for FT3 
         ml load cesga/2022 gcc/system gcccore/system openmpi/4.1.4 flexiblas/3.3.0 boost python/3.10.8 pybind11/2.12.0 cmake/3.27.6
 
-        echo Hola
-
         conda deactivate
         
         export INSTALL_PATH=$1
         export PATH=$PATH:$INSTALL_PATH/bin
 
-        cmake -B build/
-        
-        cmake --build build/
+        cmake -G Ninja -B build/ cmake -B build/ -DPYBIND_PATH=/opt/cesga/2022/software/Compiler/gcccore/system/pybind11/2.12.0/lib64/python3.9/site-packages/pybind11
+        ninja -C build -j $(nproc)
         cmake --install build/
     fi
 else
