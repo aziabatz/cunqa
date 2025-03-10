0dcac4ee7bc123bd40e53b4119fb4c021167db48
Jorge Vázquez Pérez <45230006+jorgevazquezperez@users.noreply.github.com>
2025-03-10 12:07:30 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ ! -z "$1" ]; then
-    cd ../
     if [ $LMOD_SYSTEM_NAME == "QMIO" ]; then
         # Execution for QMIO
         ml load qmio/hpc gcc/12.3.0 hpcx-ompi flexiblas/3.3.0 boost cmake/3.27.6 pybind11/2.12.0-python-3.9.9 nlohmann_json/3.11.3 ninja/1.9.0 qiskit/1.2.4-python-3.9.9
