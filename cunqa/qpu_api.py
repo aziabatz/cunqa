5866314b77374d84c86e017e274938bce2f66916
L0AA6K6vJcXTl6saLDqmOC0L+rCz3TfxpXg2HECQ+pQ= <dexposito@login03.lan.cesga.es>
2025-04-09 12:53:04 +0200

    qutils birth

--- cunqa/qpu_api.py
+++ cunqa/qpu_api.py
@@ -2,10 +2,8 @@ import os
 import sys
 import json
 import subprocess
+from cunqa.logger import logger
 from subprocess import run
-from qiskit import QuantumCircuit
-from qiskit.circuit.library import QFT
-from cunqa.circuit import qc_to_json
 from cunqa.qpu import getQPUs
 
 # Adding pyhton folder path to detect modules
@@ -16,25 +14,21 @@ class QRaiseError(Exception):
     """Exception for errors during qraise slurm command"""
     pass
 
-def create_QPU(how_many, time, flags = ''):
+def qraise(n, time, flags = ''):
     """
     Raises a QPU and returns its job_id
 
     Args
     -----------
-    how_many (int): number of QPUs to be raised
+    n (int): number of QPUs to be raised
     time (str, format: 'D-HH:MM:SS'): maximun time that the classical resources will be reserved for the QPU
-    flags (str): any other flag you want to apply. It's empty by default
+    flags (str): any other flag you want to apply, empty by default. Ex: --fakeqmio, --sim=Munich, --backend="path/to/backend", --family_name="carballido", ...
 
     """
-    
-    # assert type(how_many) == int,  f'Number must be int, but {type(how_many)} was provided' 
-    # time.replace(" ", "") #remove all whitespaces in time
-    # assert all([type(time) == str, time[0].isdecimal(), time[1].isdecimal(), time[2] == ':', time[3].isdecimal(), time[4].isdecimal(),  time[2] == ':', time[6].isdecimal(), time[7].isdecimal(), len(time) == 8]), 'Incorrect time format, it should be D-HH:MM:SS'
 
     try:
-        cmd = ["qraise", "-n", str(how_many), '-t', str(time), str(flags)]
-        # output = run(cmd, capture_output=True).stdout #run the command on terminal and capture ist output on the variable 'output'
+        cmd = ["qraise", "-n", str(n), '-t', str(time), str(flags)]
+        output = run(cmd, capture_output=True).stdout #run the command on terminal and capture ist output on the variable 'output'
 
         # job_id = ''.join(e for e in str(output) if e.isdecimal()) #checks the output on the console (looks like 'Submitted batch job 136285') and selects the number
         # return job_id
@@ -42,7 +36,7 @@ def create_QPU(how_many, time, flags = ''):
     except Exception as error:
         raise QRaiseError(f"Unable to raise requested QPUs [{error}]")
 
-def qdrop(*qpus):
+def qdrop(*families):
     """
     Drops the QPU families corresponding to the the entered QPU objects. By default, all raised QPUs will be dropped
 
@@ -51,12 +45,42 @@ def qdrop(*qpus):
     qpus (tuple(<class cunqa.qpu.QPU>)): list of QPUs to drop. All QPUs that share a qraise will these will drop
     """
     
-    if len( qpus ) == 0:
-        qpus = ['--all'] #if no QPU is provided we drop all QPU slurm jobs
+    #if no QPU is provided we drop all QPU slurm jobs
+    if len( families ) == 0:
+        qpus = ['--all'] 
 
+
+    # path to access to json file holding information about the raised QPUs
+    info_path = os.getenv("INFO_PATH")
+    if info_path is None:
+        STORE = os.getenv("STORE")
+        info_path = STORE+"/.api_simulator/qpus.json"
+
+
+    #access the large dictionary containing all QPU dictionaries
+    try:
+        with open(info_path, "r") as qpus_json:
+            dumps = json.load(qpus_json)
+
+    except Exception as error:
+        logger.error(f"Some exception occurred while retrieving the raised QPUs [{type(error).__name__}].")
+        raise SystemExit # User's level
+    
+    logger.debug(f"qpu.json file accessed correctly.")
+
+
+    #building the terminal command to drop the specified families
     cmd = ['qdrop']
-    for qpu in qpus: # Right now if two qpus have the same job_id it will be printed twice in the command. This is not an issue. The design is lazy as I think we'll go for a diff approach and I wanna minimize useless work
-        job_id = qpu.port[0:6] #Port looks like 136838_0, where the first part is the job id and the second part is the QPU id. We discard the QPU id
-        cmd.append(str(job_id))
+    if len(dumps) != 0:
+        for family in families:
+            for _, dictionary in dumps.items():
+                if dictionary.get("family").get("family_name") == family:
+                    job_id=dictionary.get("family").get("slurm_job_id")
+                    cmd.append(str(job_id)) 
+                    break #pass to the next family name
+    else:
+        logger.debug(f"qpus.json is empty, the specified families must have.")
+        
+    
     run(cmd) #run 'qdrop slurm_job_id_1 slurm_job_id_2 etc' on terminal
     return
