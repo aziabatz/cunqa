4e4a72893f5ed19de7cba42831ca02f75d0c55a5
L0AA6K6vJcXTl6saLDqmOC0L+rCz3TfxpXg2HECQ+pQ= <dexposito@login01.lan.cesga.es>
2025-04-14 08:53:15 +0200

    import error fix 3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parents[3]))
 
-
+import cunqa
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
