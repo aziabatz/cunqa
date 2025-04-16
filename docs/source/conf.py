70d539e0d065a41e30bab784cd6cb9ffd6cbde3f
L0AA6K6vJcXTl6saLDqmOC0L+rCz3TfxpXg2HECQ+pQ= <dexposito@login01.lan.cesga.es>
2025-04-16 10:11:52 +0200

    try to make the jupyter gallery work 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,7 @@
 # add these directories to sys.path here.
 import sys
 import os
+import shutil
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
@@ -26,7 +27,7 @@ release = '0.1.0'
 
 extensions = ['sphinx.ext.githubpages',  'sphinx.ext.doctest', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx_mdinclude', "nbsphinx", 'sphinx.ext.mathjax',] # 'myst_parser', 'sphinx_mdinclude', this ones were troublesome
 
-autodoc_mock_imports = ['os', 'json', 'JSONDecodeError', 'load', 'logger', 'qiskit', 'dateutil', 'glob','argparse','logging', 'numpy', 'cunqa.qclient', 'QClient', 'cunqa.mappers', 'cunqa.logger', 'qmiotools', 'qiskit_aer', ]
+autodoc_mock_imports = ['os', 'json', 'JSONDecodeError', 'load', 'logger', 'qiskit', 'dateutil', 'glob','argparse','logging', 'numpy', 'cunqa.qclient', 'QClient', 'cunqa.mappers', 'cunqa.logger', 'qmiotools', 'qiskit_aer', 'Pandoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -66,3 +67,17 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
+
+#Copy jupyter notebooks to folder docs/examples so nbsphinx can read them for our gallery (these will be ignored to avoid duplication)
+
+def setup(app):
+    print('Setup is being run')
+    here = Path(__file__).resolve()
+    project_root = here.parents[2]  # Goes from /docs/ to project root
+    source_notebooks_dir = project_root / 'examples'
+    dest_dir = here.parent / 'examples'  # This will be docs/examples
+
+    dest_dir.mkdir(exist_ok=True)
+
+    for notebook in source_notebooks_dir.glob('*.ipynb'):
+        shutil.copy(notebook, dest_dir / notebook.name)
