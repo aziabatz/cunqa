dc123a56e3a9679c53caf012244b8ce951dac99a
L0AA6K6vJcXTl6saLDqmOC0L+rCz3TfxpXg2HECQ+pQ= <dexposito@login02.lan.cesga.es>
2025-05-22 15:20:56 +0200

    playing around paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@ import os
 import shutil
 from pathlib import Path
 sys.path.insert(0, os.path.abspath('../..'))
-print(os.path.abspath('../..'))
+print(os.path.abspath('../../..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
