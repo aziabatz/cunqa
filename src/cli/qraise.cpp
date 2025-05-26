fe1a325f3a0efa716c004a8bcf2d0dc956ac45fe
acarballido <varocarballido1996@gmail.com>
2025-05-26 09:38:57 +0200

    Removed CUNQA_INFO_PATH enviroment variable set in qraise and the function get_others_endpoints() because was not used

--- src/cli/qraise.cpp
+++ src/cli/qraise.cpp
@@ -31,11 +31,6 @@ int main(int argc, char* argv[])
 
     const char* store = std::getenv("STORE");
     std::string info_path = std::string(store) + "/.cunqa/qpus.json";
-    int set_variable = setenv("CUNQA_INFO_PATH", info_path.c_str(), 1); // 1 = overwrite if exists
-    if (set_variable != 0) {
-        LOGGER_ERROR("Enviroment variable CUNQA_INFO_PATH impossible to set.");
-        return 1;
-    }
 
     std::ofstream sbatchFile("qraise_sbatch_tmp.sbatch");
     LOGGER_DEBUG("Temporal file qraise_sbatch_tmp.sbatch created.");
