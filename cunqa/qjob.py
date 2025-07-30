5bf33d8b065040c2e76aa4c14dc0b0700a6c9243
Jorge Vázquez Pérez <jorge.vazper@gmail.com>
2025-07-30 08:30:40 +0200

    Minor bug solving.

--- cunqa/qjob.py
+++ cunqa/qjob.py
@@ -279,7 +279,7 @@ class QJob:
                 "method":"statevector", 
                 "num_clbits": self.num_clbits, 
                 "num_qubits": self.num_qubits, 
-                "seed": 188}
+                "seed": 123123}
 
             if (run_parameters == None) or (len(run_parameters) == 0):
                 logger.debug("No run parameters provided, default were set.")
