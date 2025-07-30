23b35c25223a11b99e9b81b3f2c822106a335b26
Marta Losada <155301088+martalosada@users.noreply.github.com>
2025-07-30 14:57:24 +0200

    Update logger.py to WARN

--- cunqa/logger.py
+++ cunqa/logger.py
@@ -44,12 +44,12 @@ class ColoredFormatter(logging.Formatter):
         return message
 
 logger = logging.getLogger('mi_logger_coloreado')
-logger.setLevel(logging.DEBUG)  # Establece el nivel mínimo de log
+logger.setLevel(logging.WARN)  # Establece el nivel mínimo de log
 logger.propagate = False
 logger.handlers.clear()
 
 console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.WARN)
 
 formatter = ColoredFormatter('\t%(levelname)s: [%(filename)s] %(message)s\n')
 console_handler.setFormatter(formatter)
