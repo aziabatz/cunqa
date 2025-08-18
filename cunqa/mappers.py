11ced06d92ba50b0a74f88817e37036d58a6b8ed
marta <mlosada@cesga.es>
2025-08-18 08:58:32 +0200

    Updating cunqa.mappers.QJobMapper class ...

--- cunqa/mappers.py
+++ cunqa/mappers.py
@@ -167,7 +167,11 @@ class QJobMapper:
 
     An example is shown below, once we have a list of :py:class:`~cunqa.qjob.QJob` objects as *qjobs*:
 
-    >>> new_parameters = []
+
+    >>> mapper = QJobMapper(qjobs)
+    >>> # defining the parameters set accordingly to the number of parameters
+    >>> # of the circuit and the number of QJobs in the list.
+    >>> new_parameters = [...]
     >>> 
     >>> # defining the cost function passed to the result of each QJob
     >>> def cost_function(result):
