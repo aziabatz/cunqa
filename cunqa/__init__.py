e3225f92b552fe23821f07b37d30c4e5d2c606e5
marta <mlosada@cesga.es>
2025-08-28 15:09:09 +0200

    Updating cunqa ...

--- cunqa/__init__.py
+++ cunqa/__init__.py
@@ -2,3 +2,5 @@ from cunqa.qjob import QJob, gather
 from cunqa.qutils import get_QPUs
 from cunqa.transpile import transpiler
 from cunqa.mappers import QJobMapper, QPUCircuitMapper
+
+"""Description"""
