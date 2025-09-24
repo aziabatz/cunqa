0ef140289b0df427e3cf4bed679b5d121427689a
marta <mlosada@cesga.es>
2025-09-24 08:39:01 +0200

    Adding QPE QFT examples.

---
+++ examples/CESGA_use_cases/Shots-parallelization/launch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ml load qmio/hpc gcc/12.3.0 hpcx-ompi flexiblas/3.3.0 boost cmake/3.27.6 pybind11/2.12.0-python-3.9.9 nlohmann_json/3.11.3 ninja/1.9.0 qiskit/1.2.4-python-3.9.9
+ml load qmio-tools/0.2.0-python-3.9.9
+
+mode=$1
+repetitions=100
+cores_per_qpu=4
+mem=$(($cores_per_qpu * 8))
+
+for ((n=50; n<=repetitions; n++)); do
+
+    qraise -n $n -t 20:00:00 -c $cores_per_qpu --mem-per-qpu=${mem}G --cloud --fakeqmio=/opt/cesga/qmio/hpc/calibrations/2025_04_02__12_00_02.json --family_name=$mode-$n
+
+    sleep 30
+
+    sbatch --job-name="$mode-$n" \
+        --output="./logs/$mode-$n-%j.out" \
+        --error="./logs/$mode-$n-%j.err" \
+        -t 20:00:00 \
+        -c 2\
+        --mem-per-cpu 4\
+        --wrap="python -u $mode.py --num_qpus \"$n\" --cores \"$cores_per_qpu\""
+    
+    sleep 15
+done
