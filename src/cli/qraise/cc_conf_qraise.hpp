24b02306210bded0855c9b9410d4e18447dddcad
Blackburn <ahmed.ziabat@protonmail.com>
2025-10-31 08:33:15 +0100

    slurmless qraise config (classical & quantum comms)

--- src/cli/qraise/cc_conf_qraise.hpp
+++ src/cli/qraise/cc_conf_qraise.hpp
@@ -2,11 +2,13 @@
 
 #include <string>
 #include <any>
+#include <cstdlib>
 
 #include "argparse.hpp"
 #include "utils/constants.hpp"
 #include "args_qraise.hpp"
 #include "logger.hpp"
+#include "utils_qraise.hpp"
 
 
 std::string get_cc_run_command(const CunqaArgs& args, const std::string& mode)
@@ -32,16 +34,35 @@ std::string get_cc_run_command(const CunqaArgs& args, const std::string& mode)
         LOGGER_DEBUG("Qraise with classical communications and default CunqaSimulator backend. \n");
     }
 
-    #ifdef USE_MPI_BTW_QPU
+#ifdef USE_MPI_BTW_QPU
+#ifdef CUNQA_SLURMLESS
+    std::string mpi_cmd = "mpirun --allow-run-as-root -np " + std::to_string(args.n_qpus);
+    if (const char* extra = std::getenv("CUNQA_MPI_FLAGS")) {
+        mpi_cmd += " ";
+        mpi_cmd += extra;
+    }
+    run_command = mpi_cmd + " setup_qpus $INFO_PATH " +  subcommand;
+#else
     run_command =  "srun --mpi=pmix --task-epilog=$EPILOG_PATH setup_qpus $INFO_PATH " +  subcommand;
+#endif
     LOGGER_DEBUG("Run command with MPI comm: {}", run_command);
-    #endif
+#endif
 
-    #ifdef USE_ZMQ_BTW_QPU
+#ifdef USE_ZMQ_BTW_QPU
     int num_ports = args.n_qpus * 2;
+#ifdef CUNQA_SLURMLESS
+    std::string port_range = allocate_port_range(num_ports);
+    std::string mpi_cmd = "mpirun --allow-run-as-root -np " + std::to_string(args.n_qpus);
+    if (const char* extra = std::getenv("CUNQA_MPI_FLAGS")) {
+        mpi_cmd += " ";
+        mpi_cmd += extra;
+    }
+    run_command = "CUNQA_PORT_RANGE=" + port_range + " SLURM_STEP_RESV_PORTS=" + port_range + " " + mpi_cmd + " setup_qpus $INFO_PATH " +  subcommand;
+#else
     run_command =  "srun --resv-ports=" + std::to_string(num_ports) + " --task-epilog=$EPILOG_PATH setup_qpus $INFO_PATH " +  subcommand;
+#endif
     LOGGER_DEBUG("Run command with ZMQ comm: {}", run_command);
-    #endif
+#endif
 
     return run_command;
 }
--- src/cli/qraise/qc_conf_qraise.hpp
+++ src/cli/qraise/qc_conf_qraise.hpp
@@ -5,6 +5,7 @@
 #include "args_qraise.hpp"
 #include "utils_qraise.hpp"
 #include "logger.hpp"
+#include <cstdlib>
 
 std::string get_qc_run_command(const CunqaArgs& args, const std::string& mode)
 {
@@ -26,11 +27,31 @@ std::string get_qc_run_command(const CunqaArgs& args, const std::string& mode)
     int simulator_n_cores = args.cores_per_qpu * args.n_qpus; 
     int simulator_memory = args.mem_per_qpu.has_value() ? args.mem_per_qpu.value() * args.n_qpus : get_default_mem_per_core() * args.cores_per_qpu * args.n_qpus;
     
+#ifdef CUNQA_SLURMLESS
+    std::string qpu_mpi_cmd = "mpirun --allow-run-as-root -np " + std::to_string(args.n_qpus);
+    if (const char* extra = std::getenv("CUNQA_MPI_FLAGS")) {
+        qpu_mpi_cmd += " ";
+        qpu_mpi_cmd += extra;
+    }
+    std::string qpu_port_range = allocate_port_range(num_ports);
+    run_command =  "CUNQA_PORT_RANGE=" + qpu_port_range + " SLURM_STEP_RESV_PORTS=" + qpu_port_range + " " + qpu_mpi_cmd + " setup_qpus $INFO_PATH " +  subcommand + " &\n";
+
+    run_command += "sleep 1\n";
+
+    std::string executor_mpi_cmd = "mpirun --allow-run-as-root -np 1";
+    if (const char* extra = std::getenv("CUNQA_MPI_FLAGS")) {
+        executor_mpi_cmd += " ";
+        executor_mpi_cmd += extra;
+    }
+    std::string executor_port_range = allocate_port_range(args.n_qpus);
+    run_command +=  "CUNQA_PORT_RANGE=" + executor_port_range + " SLURM_STEP_RESV_PORTS=" + executor_port_range + " " + executor_mpi_cmd + " setup_executor " + args.simulator + " " + args.family_name + "\n";
+#else
     run_command =  "srun -n " + std::to_string(args.n_qpus) + " -c 1 --mem-per-cpu=1G --resv-ports=" + std::to_string(num_ports) + " --exclusive --task-epilog=$EPILOG_PATH setup_qpus $INFO_PATH " +  subcommand + " &\n";
 
     // This is done to avoid run conditions in the IP publishing of the QPUs for the executor
     run_command += "sleep 1\n";
     run_command +=  "srun -n 1 -c " + std::to_string(simulator_n_cores) + " --mem=" + std::to_string(simulator_memory) + "G --resv-ports=" + std::to_string(args.n_qpus) + " --exclusive setup_executor " + args.simulator + " " + args.family_name + "\n";
+#endif
 
     #ifndef USE_ZMQ_BTW_QPU
         LOGGER_ERROR("For quantum communications ZMQ has to be available.");
